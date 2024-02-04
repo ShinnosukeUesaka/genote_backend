d0e6b9078d28dd87db2f09bfebe53cbb6a9b4384
ShinnosukeUesaka <shinnosuke.uesaka@gmail.com>
2024-02-04 13:39:00 -0800

    Fix note update bug

--- genote_llm/main.py
+++ genote_llm/main.py
@@ -154,7 +154,8 @@ def add_notes(user_id: str, draft_input: DraftInput):
                 add_notes_to_rag([{"id": note.id, "data": note.to_dict()}])
             else:
                 note = db.collection("users").document(user_id).collection("notes").document(note["id"])
-                note = note.update({"content": action["content"], "status": "edited", "order": -1})[1].get()
+                note.update({"content": action["content"], "status": "edited", "order": -1})
+                note = note.get()
                 update_note_to_rag({"id": note.id, "data": note.to_dict()})
         elif action["method"] == "add":
             note = db.collection("users").document(user_id).collection("notes").add({"title": action["title"], "content": action["content"], "status": "added", "order": -2})[1].get()
